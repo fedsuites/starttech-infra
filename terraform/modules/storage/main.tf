ce0a012e8ad93d24ab2cd000152c0973aff63477
fedsuites <suitesfed@gmail.com>
2026-05-20 16:07:59 +0100

    fix: remove remaining cloudfront references

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -55,11 +55,11 @@ resource "aws_s3_bucket_policy" "frontend" {
         }
         Action   = "s3:GetObject"
         Resource = "${aws_s3_bucket.frontend.arn}/*"
-        Condition = {
+        /*Condition = {
           StringEquals = {
             "AWS:SourceArn" = aws_cloudfront_distribution.frontend.arn
           }
-        }
+        }*/
       }
     ]
   })
